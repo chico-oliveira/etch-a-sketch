3c448f16da10fe5506ff1cf69f473cc97a639fae
Francisco Oliveira <chicinho.oliveira@gmail.com>
2021-11-18 05:12:40 +0000

    Create grid with Flex

--- scripts/app.js
+++ scripts/app.js
@@ -2,6 +2,12 @@
 // VARIABLES //
 ///////////////
 
+const gridSquared = document.querySelector('.grid-grid');
+const gridFlex = document.querySelector('.grid-flex');
+const gridCount = 2; //2*2
+const gridSize = 496;
+const elementSize = (gridSize/gridCount);
+
 /////////////////
 // DOM METHODS //
 /////////////////
@@ -9,3 +15,25 @@
 ///////////////
 // FUNCTIONS //
 ///////////////
+
+for (let i=0; i<(gridCount ** 2); i++){
+    let gridDiv = document.createElement('div');
+    gridDiv.classList.add('gridElement');
+    gridDiv.style.borderStyle = "solid";
+    gridDiv.style.borderWidth = "2px";
+    gridDiv.style.borderColor = "black";
+    gridDiv.style.width = `${elementSize}px`;
+    gridDiv.style.height = `${elementSize}px`;
+    gridFlex.appendChild(gridDiv);
+}
+
+for (let i=0; i<(gridCount ** 2); i++){
+    let gridDiv = document.createElement('div');
+    gridDiv.classList.add('gridElement');
+    gridDiv.style.borderStyle = "solid";
+    gridDiv.style.borderWidth = "2px";
+    gridDiv.style.borderColor = "black";
+    gridDiv.style.width = `${elementSize}px`;
+    gridDiv.style.height = `${elementSize}px`;
+    gridSquared.appendChild(gridDiv);
+}
