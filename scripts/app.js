f8c3a4a00d966fe8e0445e8564dae92946d6e533
Francisco Oliveira <chicinho.oliveira@gmail.com>
2021-11-19 02:05:52 +0000

    Create grid with Grid Display

--- scripts/app.js
+++ scripts/app.js
@@ -3,10 +3,10 @@
 ///////////////
 
 const gridSquared = document.querySelector('.grid-grid');
-const gridFlex = document.querySelector('.grid-flex');
-const gridCount = 2; //2*2
-const gridSize = 496;
-const elementSize = (gridSize/gridCount);
+const gridFlex = document.querySelector('.grid-flex'); // Flex Only
+const gridLength = 16; //2*2
+const gridSize = 500;
+const elementSize = ((gridSize-4)/gridLength); // Flex Only
 
 /////////////////
 // DOM METHODS //
@@ -16,24 +16,30 @@ const elementSize = (gridSize/gridCount);
 // FUNCTIONS //
 ///////////////
 
-for (let i=0; i<(gridCount ** 2); i++){
+// Update CSS variables
+let root = document.documentElement;
+root.style.setProperty("--grid-length", gridLength);
+root.style.setProperty("--grid-size", gridSize+"px");
+
+// Grid
+for (let i=0; i<(gridLength ** 2); i++){
     let gridDiv = document.createElement('div');
     gridDiv.classList.add('gridElement');
     gridDiv.style.borderStyle = "solid";
-    gridDiv.style.borderWidth = "2px";
+    gridDiv.style.borderWidth = "1px";
+    gridDiv.style.borderColor = "black";
+    gridSquared.appendChild(gridDiv);
+}
+
+
+// Flex
+for (let i=0; i<(gridLength ** 2); i++){
+    let gridDiv = document.createElement('div');
+    gridDiv.classList.add('gridElement');
+    gridDiv.style.borderStyle = "solid";
+    gridDiv.style.borderWidth = "1px";
     gridDiv.style.borderColor = "black";
     gridDiv.style.width = `${elementSize}px`;
     gridDiv.style.height = `${elementSize}px`;
     gridFlex.appendChild(gridDiv);
-}
-
-for (let i=0; i<(gridCount ** 2); i++){
-    let gridDiv = document.createElement('div');
-    gridDiv.classList.add('gridElement');
-    gridDiv.style.borderStyle = "solid";
-    gridDiv.style.borderWidth = "2px";
-    gridDiv.style.borderColor = "black";
-    gridDiv.style.width = `${elementSize}px`;
-    gridDiv.style.height = `${elementSize}px`;
-    gridSquared.appendChild(gridDiv);
 }
