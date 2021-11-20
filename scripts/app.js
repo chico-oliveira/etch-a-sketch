0ae8d38bf12acebcc03f4ef6460f712d50a4f017
Francisco Oliveira <chicinho.oliveira@gmail.com>
2021-11-20 21:03:33 +0000

    Add functional different modes

--- scripts/app.js
+++ scripts/app.js
@@ -4,23 +4,28 @@
 
 const DEFAULT_GRID_LENGTH = 8;
 const DEFAULT_COLOUR = "blue";
-const DEFAULT_BRIGHTNESS = 100;
+const DEFAULT_MODE = "standard";
 
 const root = document.documentElement;
 const gridSquared = document.querySelector('.grid');
 const sizeText = document.querySelector('.grid-length');
 const clearButton = document.getElementById("clear");
+const modeButtons = document.querySelectorAll('.mode');
+let divs = document.querySelectorAll(".gridElement");
 
 let gridLength = DEFAULT_GRID_LENGTH;
-let selectedColour = DEFAULT_COLOUR;
+let standardColour = DEFAULT_COLOUR;
 let currentColour = DEFAULT_COLOUR;
-let brightness = DEFAULT_BRIGHTNESS;
+let currentMode = DEFAULT_MODE;
 
 /////////////////
 // DOM METHODS //
 /////////////////
 
 clearButton.onclick = clearGrid;
+modeButtons.forEach(button => {
+    button.addEventListener('click', activateMode);
+});
 
 ///////////////
 // FUNCTIONS //
@@ -48,6 +53,9 @@ function loadGrid() {
         gridDiv.addEventListener("mouseenter", colour);
     }
 
+    // Updates grid elements
+    divs = document.querySelectorAll(".gridElement")
+
     // Updates length display
     sizeText.textContent = `${gridLength} × ${gridLength}`;
 }
@@ -60,11 +68,20 @@ function clearGrid(){
 
 function updateCSS(){
     root.style.setProperty("--grid-length", gridLength);
-    root.style.setProperty("--selected-colour", currentColour);
 }
 
 function colour(e){
-    randomColour(e);
+    if(currentMode === "standard"){
+        currentColour = standardColour;
+    }
+    else if(currentMode === "rainbow"){
+        currentColour = randomColour();
+    }
+    else {
+        currentColour = greyScale(e.target);
+    }
+
+    // Paints div
     e.target.style.backgroundColor = currentColour;
 }
 
@@ -73,28 +90,47 @@ function randomColour(){
     let g = Math.floor(Math.random()*255);
     let b = Math.floor(Math.random()*255);
 
-    currentColour = `rgb(${r}, ${g}, ${b})`;
-    updateCSS();
+    return `rgb(${r}, ${g}, ${b})`;
 }
 
-function greyScale(e){
-    let bgColour = e.target.style.backgroundColor;
+function greyScale(div){
+    let bgColour = div.style.backgroundColor;
     
     // Checks if the grid element has already started a gradient
-    if (e.target.classList.length < 2){
-        e.target.classList.add("grey");
-        currentColour = "rgba(0, 0, 0, 0.1)";
+    if (div.classList.length < 2){
+        div.classList.toggle("grey", true);
+        return "rgba(0, 0, 0, 0.1)";
     }
     // Mid-gradient, increment alpha
     else if (bgColour.indexOf("rgba") !== -1) {
         let alpha = Number(bgColour.slice(-4, -1));
         if (alpha < 1){
             alpha += 0.1;
-            currentColour = `rgba(0, 0, 0, ${alpha})`;
+            return `rgba(0, 0, 0, ${alpha})`;
         }
     }
     // If the gradient has finished, square stays black
     else {
-        currentColour = "black";
+        return "black";
+    }
+}
+
+function activateMode(e){
+    // Deselects all buttons and then activates one
+    modeButtons.forEach(button => {
+        button.classList.toggle("selected", false);
+    });
+    e.target.classList.toggle("selected", true);
+
+    // Activates colour mode
+    buttonText = e.target.textContent;
+    let mode = buttonText.substring(0, buttonText.indexOf(' '));
+    currentMode = mode.toLowerCase();
+
+    // Removes grey-scale 
+    if(currentMode !== "greyscale"){
+        divs.forEach(element => {
+            element.classList.toggle("grey", false);
+        });
     }
 }
